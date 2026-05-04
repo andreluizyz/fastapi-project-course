9c882f010cb1070fd325f9d8ae5464022a06eb8b
André Luiz <alfonsecafilho@gmail.com>
2026-05-04 11:21:49 -0300

    Commit

--- auth_routes.py
+++ auth_routes.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter
-from models import User, db
-from sqlalchemy.orm import sessionmaker
+from fastapi import APIRouter, Depends, HTTPException
+from models import User
+from dependencies import get_session
+from main import bcrypt_context
+from schemas import UserSchema
+from sqlalchemy.orm import Session
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -12,14 +15,14 @@ async def authenticate():
     return {"mensagem": "Você acessou a rota padrão de autenticação", "autenticado": False}
 
 @auth_router.post("/create_user")
-async def create_user(email: str, password: str, name: str, number: str):
-    Session = sessionmaker(bind=db)
-    session = Session()
-    user = session.query(User).filter(User.email == email).first()
+async def create_user(user_schema: UserSchema, session: Session = Depends(get_session)):
+    user = session.query(User).filter(User.email == user_schema.email).first()
     if user:
-        return {"mensagem": "User alredy exists"}
+        return HTTPException(status_code=400, detail="User email already exists")
     else:
-        new_user = User(name=name, email=email, password=password, number=number) 
+        crypt_password = bcrypt_context.hash(user_schema.passsword)
+        new_user = User(name=user_schema.name, email=user_schema.email, password=crypt_password, number=user_schema.number, status=user_schema.status, admin=user_schema.admin) 
         session.add(new_user)
         session.commit()
-        return {"mensagem": "User created sucessfully"}
+        return {"mensagem": f"User created sucessfully {user_schema.email}"}
+    
