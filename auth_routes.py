6bbf83b6128dd4328409bedf2aff48a7747eea40
André Luiz <alfonsecafilho@gmail.com>
2026-05-04 09:30:32 -0300

    Up 2

--- auth_routes.py
+++ auth_routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from models import User
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
@@ -9,3 +10,6 @@ async def authenticate():
     """
     return {"mensagem": "Você acessou a rota padrão de autenticação", "autenticado": False}
 
+@auth_router.post("/create_user")
+async def create_user(email: str, password: str):
+    user = User.name
